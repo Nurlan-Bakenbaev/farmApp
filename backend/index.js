8c06a68c15c888d9ac16577ba3ed94a2bf8ebddf
Nurlan <iamsamurai1991@gmail.com>
2024-10-24 22:26:53 +0200

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { connectionDB } from "./config/db.js";
-import productRouter from "./routes/productRouter.js";
-import userRouter from "./routes/userRouter.js";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { connectionDB } from './config/db.js';
+import productRouter from './routes/productRouter.js';
+import userRouter from './routes/userRouter.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -14,22 +14,24 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const allowedOrigins = [
-  "http://localhost:3000",
-  "https://farm-app-lime.vercel.app",
+ 'http://localhost:3000',
+ 'https://farm-app-lime.vercel.app',
+ 'https://farm-1cpj66w15-nurlan-bakenbaevs-projects.vercel.app',
+ 'https://farm-app-git-main-nurlan-bakenbaevs-projects.vercel.app/'
 ];
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
+ cors({
+  origin: function (origin, callback) {
+   if (!origin) return callback(null, true);
 
-      if (allowedOrigins.indexOf(origin) === -1) {
-        return callback(new Error("Not allowed by CORS"));
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  })
+   if (allowedOrigins.indexOf(origin) === -1) {
+    return callback(new Error('Not allowed by CORS'));
+   }
+   return callback(null, true);
+  },
+  credentials: true
+ })
 );
 
 // Middleware for parsing JSON and URL-encoded data
@@ -38,18 +40,18 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the uploads directory
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use("/user", userRouter);
-app.use("/api", productRouter);
+app.use('/user', userRouter);
+app.use('/api', productRouter);
 
 // Connect to MongoDB and start the server
 connectionDB(process.env.MONGO_DB_URL)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Database connection failed:", err);
+ .then(() => {
+  app.listen(process.env.PORT, () => {
+   console.log(`Server is running on ${process.env.PORT}`);
   });
+ })
+ .catch((err) => {
+  console.error('Database connection failed:', err);
+ });
