9488cf55081b1282cf63d729ac43b68469a16057
Nurlan <iamsamurai1991@gmail.com>
2024-10-24 14:49:34 +0200

    modified:   backend/controllers/userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -45,7 +45,7 @@ export const signIn = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ email }).populate("+products").select("+password");
+    const user = await User.findOne({ email }).select("+password");
     if (!user) {
       return res
         .status(404)
