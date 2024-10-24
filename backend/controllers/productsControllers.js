26f2b17010cac876198837a07ab61e14f81cb33d
Nurlan <iamsamurai1991@gmail.com>
2024-10-24 21:44:54 +0200

    modified:   backend/controllers/productsControllers.js

--- backend/controllers/productsControllers.js
+++ backend/controllers/productsControllers.js
@@ -3,28 +3,22 @@ import Product from '../models/product.model.js';
 import { fileURLToPath } from 'url';
 import fs from 'fs/promises';
 import User from '../models/usersModel.js';
-// POST PRODUCT
 export const postProduct = async (req, res) => {
  const product = req.body;
-
  if (!product.name || !product.userId) {
   return res.status(400).json({ success: false, message: 'Please provide all fields' });
  }
-
  const imagePaths = req.files.map((file) => file.path);
  const newProduct = new Product({
   ...product,
   user: product.userId,
   images: imagePaths
  });
-
  try {
   await newProduct.save();
-
   await User.findByIdAndUpdate(product.userId, {
    $push: { products: newProduct._id }
   });
-
   return res.status(201).json({
    success: true,
    message: 'Product added successfully',
@@ -35,10 +29,8 @@ export const postProduct = async (req, res) => {
   return res.status(500).json({ success: false, error: error.message });
  }
 };
-
 export const getOneProduct = async (req, res) => {
  const { id } = req.params;
-
  try {
   const singleProduct = await Product.findById(id).populate('user');
   if (!singleProduct) {
@@ -50,7 +42,6 @@ export const getOneProduct = async (req, res) => {
   return res.status(500).json({ success: false, error: error.message });
  }
 };
-
 export const getAllProducts = async (req, res) => {
  try {
   const products = await Product.find();
@@ -60,19 +51,16 @@ export const getAllProducts = async (req, res) => {
   return res.status(500).json({ success: false, error: error.message });
  }
 };
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const deleteProduct = async (req, res) => {
  const { id } = req.params;
-
  try {
   const product = await Product.findById(id);
   if (!product) {
    return res.status(404).json({ success: false, message: 'Product not found' });
   }
-
   if (product.images && product.images.length > 0) {
    await Promise.all(
     product.images.map(async (imagePath) => {
@@ -86,7 +74,6 @@ export const deleteProduct = async (req, res) => {
     })
    );
   }
-
   await Product.findByIdAndDelete(id);
   return res.status(200).json({
    success: true,
@@ -101,18 +88,15 @@ export const deleteProduct = async (req, res) => {
   });
  }
 };
-
 export const updateProduct = async (req, res) => {
  const { id } = req.params;
  const { name, price, img } = req.body.product;
-
  try {
   const updatedProduct = await Product.findByIdAndUpdate(id, { name, price, img }, { new: true, runValidators: true });
 
   if (!updatedProduct) {
    return res.status(404).json({ success: false, message: 'Product not found' });
   }
-
   return res.status(200).json({
    success: true,
    message: 'Product updated successfully',
@@ -123,10 +107,8 @@ export const updateProduct = async (req, res) => {
   return res.status(500).json({ success: false, message: error.message });
  }
 };
-
 export const searchProducts = async (req, res) => {
  const { searchTerm } = req.query;
-
  try {
   const products = await Product.find({
    $or: [{ name: { $regex: searchTerm, $options: 'i' } }, { description: { $regex: searchTerm, $options: 'i' } }]
@@ -138,20 +120,5 @@ export const searchProducts = async (req, res) => {
  } catch (error) {
   console.error('Error searching products:', error);
   return res.status(500).json({ success: false, error: error.message });
- }
-};
-
-export const getManyById = async (req, res) => {
- const { ids } = req.query;
- console.log(ids);
- try {
-  if (!ids) {
-   return res.status(400).json({ success: false, message: 'No IDs provided' });
-  }
-  const idArray = ids.split(',');
-  const likedProducts = await Product.find({ _id: { $in: ids } });
-  return res.status(200).json({ success: true, quantity: likedProducts.length, likedProducts });
- } catch (error) {
-  res.status(500).json({ error: error.message });
  }
 };
