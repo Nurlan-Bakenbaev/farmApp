16cbc7e87f48cb9a8e8f0dc91958918647ac3a12
Nurlan <iamsamurai1991@gmail.com>
2024-10-27 12:00:20 +0100

    modified:   backend/routes/productRouter.js

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -2,11 +2,10 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { getAllProducts, updateProduct, deleteProduct, postProduct, getOneProduct, searchProducts, getManyById } from '../controllers/productsControllers.js';
+import { getAllProducts, updateProduct, deleteProduct, postProduct, getOneProduct, searchProducts } from '../controllers/productsControllers.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const productRouter = express.Router();
-
 const storage = multer.diskStorage({
  destination: (req, file, cb) => {
   cb(null, path.join(__dirname, '../uploads'));
@@ -19,7 +18,5 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 productRouter.route('/product').get(getAllProducts).post(upload.array('files', 10), postProduct);
 productRouter.route('/product/:id').patch(updateProduct).delete(deleteProduct).get(getOneProduct);
-productRouter.get('/likedproducts', getManyById);
 productRouter.get('/search', searchProducts);
-
 export default productRouter;
