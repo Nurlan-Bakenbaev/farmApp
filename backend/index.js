e54d7c070cedfe4a6ab55395bb808bb6c1f6cb2f
Nurlan <iamsamurai1991@gmail.com>
2024-10-27 13:32:43 +0100

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,7 @@ const allowedOrigins = [
 app.use(
  cors({
   origin: function (origin, callback) {
+   console.log('Request Origin:', origin); 
    if (!origin) return callback(null, true);
 
    if (allowedOrigins.indexOf(origin) === -1) {
